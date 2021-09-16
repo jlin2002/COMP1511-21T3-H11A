cf6f2c997443c06bf23fa69b58d60aebf6e006b4
Joanna Lin <linjoanna31@gmail.com>
2021-09-16 13:13:40 +1000

    Added tut01 files and slides

---
+++ tut01/face0.c
@@ -0,0 +1,12 @@
+// Program that prints a face
+// H11A, September 2021
+
+#include <stdio.h>
+
+int main(void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+    return 0;
+}
